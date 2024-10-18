4910d27e0fbeefc78031a6416dd0ee091e349d09
private <155294690+aswinxd@users.noreply.github.com>
2024-10-18 21:07:48 +0530

    Update rsgo.py

--- rsgo.py
+++ rsgo.py
@@ -56,7 +56,7 @@ async def run_session():
             await bot.send_photo(channel, edited_image, caption=caption, reply_markup=markup)
             await asyncio.sleep(round_intervals)
 
-         await bot.send_message(  # <-- Updated Line
+        await bot.send_message(  # <-- Updated Line
             channel,
             f"📊 **Session Summary**: \nTotal winnings after 5 rounds: ₹{total_winnings[channel]}\nSession ended.",
             reply_markup=markup
