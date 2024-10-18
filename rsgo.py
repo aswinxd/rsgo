7c4b6973bff39a67ca16e003ab3d1d139643737f
private <155294690+aswinxd@users.noreply.github.com>
2024-10-18 15:05:18 +0530

    Update rsgo.py

--- rsgo.py
+++ rsgo.py
@@ -81,7 +81,7 @@ async def start(client, message):
         
 async def start_bot():
     await bot.start()
-    asyncio.create_task(run_session()) 
+    asyncio.create_task(schedule_sessions()) 
     await idle()  
 
 if __name__ == "__main__":
