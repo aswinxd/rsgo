10fe702ca8e8f202ebfc5d1ac01f2a49d4fa6a23
private <155294690+aswinxd@users.noreply.github.com>
2024-10-19 11:59:24 +0530

    Update rsgo.py

--- rsgo.py
+++ rsgo.py
@@ -92,7 +92,8 @@ async def run_session():
             await bot.send_photo(channel, edited_image, caption=caption, reply_markup=markup)
             await asyncio.sleep(round_intervals)
 
-        final_summary_image = edit_final_summary_image(total_winnings[channel], round_results[channel])
+       final_summary_image = edit_final_summary_image(total_winnings[channel], round_results[channel])
+         await bot.send_photo(channel, final_summary_image, caption=final_message, reply_markup=markup)
         final_message = (
             f" **Session Summary**: \n"
             f"Total winnings after 5 rounds: ₹{total_winnings[channel]}\n"
