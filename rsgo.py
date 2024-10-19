8fcd06d0a53179060bd93562a1ea0387deccb4e2
private <155294690+aswinxd@users.noreply.github.com>
2024-10-19 14:18:06 +0530

    Update rsgo.py

--- rsgo.py
+++ rsgo.py
@@ -13,7 +13,7 @@ BOT_TOKEN = "6520550784:AAHZPv8eOS2Unc91jIVYSH5PB0z8SO36lUY"
 bot = Client("aviator_betting_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 bet_amount = 1000 
-session_times = ["09:00", "11:00", "13:50", "15:00", "17:00", "19:00", "21:00", "23:00"] #international
+session_times = ["09:00", "11:00", "13:00", "15:00", "17:00", "19:00", "21:00", "23:00"] #international
 channels_to_post = ["-1002396190461"] 
 round_intervals = 60  
 def edit_image(multiplier, winnings):
@@ -160,11 +160,11 @@ async def run_session():
 async def schedule_sessions():
     while True:
         now = datetime.now().strftime("%H:%M")
-        print(f"Current Time: {now}") 
+        print(f"Current") 
         if now in session_times:
-            print(f"Starting session at {now}")  
+            print(f"Starting")  
             await run_session()
-        await asyncio.sleep(10)
+        await asyncio.sleep(30)
 
 @bot.on_message(filters.command("start"))
 async def start(client, message):
