a2905976a89c4e8a1e6952b16b739fe601b5d350
private <155294690+aswinxd@users.noreply.github.com>
2024-10-20 23:10:34 +0530

    Update rsgo.py

--- rsgo.py
+++ rsgo.py
@@ -81,7 +81,7 @@ async def run_session():
             multiplier = generate_round_result()
             winnings = calculate_winnings(bet_amount, multiplier)
             total_winnings[channel] += winnings
-            round_results[channel].append(f"Round {round_num} - Bet: **{multiplier}x ")
+            round_results[channel].append(f"Bet: {multiplier}x ")
             await bot.send_message(channel, f" Bet: **{multiplier}x**")
             await asyncio.sleep(30)
             edited_image = edit_image(multiplier, winnings)
